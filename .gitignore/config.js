925e72dcb5b850ce79c784ae35fc3c5b760b6dd4
Yashvant Dongare <yashvantnd007@gmail.com>
2026-05-03 02:10:45 +0530

    removed api totally

---
+++ .gitignore/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8Kx9S0o8J-p7647VwL8U9y-9Z4X0U-oU",
+    authDomain: "dominator-myself.firebaseapp.com",
+    projectId: "dominator-myself",
+    storageBucket: "dominator-myself.appspot.com",
+    messagingSenderId: "963254222441",
+    appId: "1:963254222441:web:8f3bd9c0846a04ccd4acf8"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
